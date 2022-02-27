54cf8388ed05166e9245d02bdfd6db808d2eeb57
Gnanendra18 <tognanendrakumar@gmail.com>
2022-02-27 19:35:17 +0530

    Screen Orientation makes the number to zero again. This is onconfiguration change which makes our activity recreate and the data will be lost.

--- app/src/main/java/com/incrediblepath/architecturecomponents/MainActivity.kt
+++ app/src/main/java/com/incrediblepath/architecturecomponents/MainActivity.kt
@@ -2,10 +2,17 @@ package com.incrediblepath.architecturecomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.incrediblepath.architecturecomponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var number = 0
+        binding.button.setOnClickListener{
+            number++
+            binding.textView.text = number.toString()
+        }
     }
 }
