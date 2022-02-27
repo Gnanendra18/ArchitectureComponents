380d48a440700043161e5b73f19317026dc39f88
Gnanendra18 <tognanendrakumar@gmail.com>
2022-02-27 21:32:31 +0530

    For sending application context

--- app/src/main/java/com/incrediblepath/architecturecomponents/MainActivityViewModel.kt
+++ app/src/main/java/com/incrediblepath/architecturecomponents/MainActivityViewModel.kt
@@ -1,8 +1,10 @@
 package com.incrediblepath.architecturecomponents
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
 
     var number  = 0
     fun incrementNumber(){
