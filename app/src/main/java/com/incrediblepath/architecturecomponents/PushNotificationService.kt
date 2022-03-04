678fb52ae85ee5f7ec5679dd72e8a30cef070edd
Gnanendra18 <tognanendrakumar@gmail.com>
2022-03-04 06:03:55 +0530

    push check

---
+++ app/src/main/java/com/incrediblepath/architecturecomponents/PushNotificationService.kt
@@ -0,0 +1,7 @@
+package com.incrediblepath.architecturecomponents
+
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class PushNotificationService: FirebaseMessagingService() {
+}
