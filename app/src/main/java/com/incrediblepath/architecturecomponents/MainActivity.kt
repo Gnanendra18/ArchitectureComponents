95ae6bfe744576f3d72b43686fc4432c9cf025ee
Gnanendra18 <tognanendrakumar@gmail.com>
2022-03-04 05:35:34 +0530

    notification

--- app/src/main/java/com/incrediblepath/architecturecomponents/MainActivity.kt
+++ app/src/main/java/com/incrediblepath/architecturecomponents/MainActivity.kt
@@ -1,32 +1,69 @@
 package com.incrediblepath.architecturecomponents
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var broadcastReceiver:AirplaneReceiver
-    lateinit var myReceiver: MyReceiver
+    val CHANNEL_ID = "some_channel_id"
+    val CHANNEL_NAME = "some_channel_name"
+    val NOTIFICATION_ID = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        broadcastReceiver = AirplaneReceiver()
-        registerReceiver(broadcastReceiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
-        myReceiver = MyReceiver()
-        registerReceiver(myReceiver, IntentFilter("My_ACTION"))
+        createNotificationChannel()
+        val notificationManager = NotificationManagerCompat.from(this)
+        val intent = Intent(this,MainActivity::class.java)
+        intent.putExtra("ARGUMENT","SEND_TO_NOTIFICATION")
+        getIntent().getStringExtra("ARGUMENT")?.let { Log.e("ARGUMENT", it) }
+        val taskStack = TaskStackBuilder.create(this)
+        taskStack.addNextIntentWithParentStack(intent)
+        val pendingIntent = taskStack.getPendingIntent(0,PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setContentTitle("THis is title")
+            .setContentText("This is discription")
+            .setSmallIcon(R.drawable.notification_bar_image)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            //.setAutoCancel(true)
+            .build()
+
+
         findViewById<TextView>(R.id.textview).setOnClickListener{
-            sendBroadcast(Intent("My_ACTION"))
+            notificationManager.notify(NOTIFICATION_ID,notification)
         }
 
 
+
     }
 
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(broadcastReceiver)
-        unregisterReceiver(myReceiver)
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            //create a notification channel
+                val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT)
+            channel.enableLights(true)
+            channel.lightColor = Color.GREEN
+
+            //val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager = NotificationManagerCompat.from(this)
+            notificationManager.createNotificationChannel(channel)
+
+        }
     }
+
+
 }
